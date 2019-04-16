91271e0970639d1676b6ca485f7402da9fec0777
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-16 19:10:35 +0000

    Deploy mazhuravlev/windows to github.com/mazhuravlev/windows.git:gh-pages

---
+++ precache-manifest.9585b5ab6992a2d59dbdca593424ed93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1faca82e01434ee6a77c",
+    "url": "/windows/static/css/main.ff252c21.chunk.css"
+  },
+  {
+    "revision": "1faca82e01434ee6a77c",
+    "url": "/windows/static/js/main.1faca82e.chunk.js"
+  },
+  {
+    "revision": "97123658b8868aab244b",
+    "url": "/windows/static/css/1.34e44f0f.chunk.css"
+  },
+  {
+    "revision": "97123658b8868aab244b",
+    "url": "/windows/static/js/1.97123658.chunk.js"
+  },
+  {
+    "revision": "3055ddf525ea997e8811",
+    "url": "/windows/static/js/runtime~main.3055ddf5.js"
+  },
+  {
+    "revision": "77dbda87a8732b735b77a5af0fff5e4e",
+    "url": "/windows/static/media/basketIcon.77dbda87.svg"
+  },
+  {
+    "revision": "30c1afad69842154ab171c19918ad87b",
+    "url": "/windows/index.html"
+  }
+];
